a1165ecd0edc79ba72648732ed7fcceac420d6e3
Fred Murigi <105373496+Freddie16@users.noreply.github.com>
2023-01-11 10:40:52 +0300

    first commit

---
+++ item.js
@@ -0,0 +1,14 @@
+function toDoItem() {
+    return(
+        <div>
+            <h3>
+                Learn React
+            </h3>
+            <p>
+                lorem ipsum
+            </p>
+        </div>
+    )
+        
+   
+}
---
+++ list.js
@@ -0,0 +1,5 @@
+import toDoItem from '.toDoItem'
+function toDoList() {
+    return(<div>toDoItem</div>)
+}
+export default toDoItem
